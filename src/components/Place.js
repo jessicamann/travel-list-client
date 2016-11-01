f894eeb145f6f24f1aed586a59e864e507bfb0b6
Jess Mann <jmann@smith.edu>
2016-11-01 16:57:40 -0400

    fixed indentation

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class Place extends React.Component {
-	render() {
-		return (
-			<div className='Place-box box-color-peach'>
-				<h2 className='Place-name'>{this.props.name}</h2>
-				<h3 className='Place-location'>Location: {this.props.location}</h3>
-				<h3 className='Place-description'>Description: {this.props.description}</h3>
-			</div>
-		);		
-	}
+  render() {
+    return (
+      <div className='Place-box box-color-peach'>
+        <h2 className='Place-name'>{this.props.name}</h2>
+        <h3 className='Place-location'>Location: {this.props.location}</h3>
+        <h3 className='Place-description'>Description: {this.props.description}</h3>
+      </div>
+    );		
+  }
 }
 
 export default Place
