4e1f3c4c441f10fd8171f27e8d3d27c93462b63c
Jess Mann <jmann@smith.edu>
2016-11-02 15:04:26 -0400

    changed tile styling and background color

--- src/components/PlaceBox.js
+++ src/components/PlaceBox.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class PlaceBox extends React.Component {
   render() {
     return (
-      <div className='Place-box box-color-peach'>
-        <h2 className='Place-name'>{this.props.name}</h2>
-        <h3 className='Place-location'>Location: {this.props.location}</h3>
-        <h3 className='Place-description'>Description: {this.props.description}</h3>
+      <div className='box box1'>
+      	<div className='dashed-box'>
+          <h2 className='Place-name'>{this.props.name}</h2>
+          <h3 className='Place-location'>Location: {this.props.location}</h3>
+          <h3 className='Place-description'>Description: {this.props.description}</h3>
+        </div>
       </div>
     );		
   }
